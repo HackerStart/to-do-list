bffe67fb5b1d4002e5dc0681b0c96ce9be8a7e11
Eason Li <lijinghua@odc.cmbchina.cn>
2019-09-27 17:21:17 +0800

    define three components

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,8 +1,17 @@
 import React from "react";
 import "./ToDoList.css";
+import Header from "./components/header/Header.js";
+import Body from "./components/body/Body.js";
+import Footer from "./components/footer/Footer.js";
 
 function ToDoList() {
-  return <div className="container"></div>;
+  return (
+    <div className="container">
+      <Header></Header>
+      <Body></Body>
+      <Footer></Footer>
+    </div>
+  );
 }
 
 export default ToDoList;
