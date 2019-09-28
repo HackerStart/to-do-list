f17508525b4cdad2cc70d79b384481e16bf1397b
Eason Li <lijinghua@odc.cmbchina.cn>
2019-09-28 11:19:58 +0800

    impletment header component

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,7 +3,14 @@ import "./Header.css";
 
 class Header extends React.Component {
   render() {
-    return <div className="header"></div>;
+    return (
+      <div className="header">
+        <h2 className="title">To Do List</h2>
+        <p className="sub-title">
+          <em>Simple Todo List with adding and filter by diff status.</em>
+        </p>
+      </div>
+    );
   }
 }
 
